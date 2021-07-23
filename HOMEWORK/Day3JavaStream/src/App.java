63c0f7f74518d58096e182169382b3d0c3ee59d7
phobo99 <phobotkcb@gmail.com>
2021-07-23 20:05:53 +0700

    [Bài tập 03] JavaStream

---
+++ HOMEWORK/Day3JavaStream/src/App.java
@@ -0,0 +1,28 @@
+public class App {
+    public static void main(String[] args) {
+        //DemoStream.map01();
+        //DemoStream.map02();
+        //DemoStream.map03();
+        //DemoStream.reduce01();
+        //DemoStream.summingInt();
+        //DemoStream.maxBy();
+        //DemoStream.minBy();
+        //DemoStream.average();
+        //DemoStream.distinct();
+        //DemoStream.groupBy01();
+        //DemoStream.averageAge();
+        //DemoStream.sortByName();
+        //DemoStream.sortByName02();
+        //DemoStream.filterOutChinese();
+        //DemoStream.peek();
+        //DemoStream.peek();
+        //DemoStream.showPeopleByAge_20to30();
+
+
+        //DemoStream.filterAge(20, 25);
+        //System.out.println(DemoStream.averageAge2());
+        //DemoStream.averageAgeByNationality();
+        //System.out.println(DemoStream.sumsPeopleByNationality());
+        DemoStream.getAverageAgeByNation();
+    }
+}
