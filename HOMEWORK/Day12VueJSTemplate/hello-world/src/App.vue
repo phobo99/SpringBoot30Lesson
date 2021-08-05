7047c338ff83b066356ceb78d708d8663e85bd03
phobo99 <phobotkcb@gmail.com>
2021-08-05 17:31:21 +0700

    [Bài tập 12]VueJS Template

---
+++ HOMEWORK/Day12VueJSTemplate/hello-world/src/App.vue
@@ -0,0 +1,191 @@
+<template>
+  <!-- <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div> -->
+
+  <div class="container">
+    <div class="row justify-content-between">
+      <h1>{{ brand }}</h1>
+      <a href="#" class="btn btn-info btn-lg">
+        <span class="fa fa-shopping-cart"></span> {{ count }}
+      </a>
+    </div>
+    <div class="row mt-5">
+      <div class="col-lg-12 card-margin">
+        <div class="card search-form">
+          <div class="card-body p-0">
+            <form id="search-form">
+              <div class="row">
+                <div class="col-12">
+                  <div class="row no-gutters">
+                    <div class="col-lg-3 col-md-3 col-sm-12 p-0 ">
+                      <select
+                        class="form-control"
+                        id="exampleFormControlSelect1" 
+                      >
+                        <option hidden>Sort by...</option>
+                        <option>Feature</option>
+                        <option>Price: High to Low</option>
+                        <option>Price: Low to High</option>
+                        <option>A - Z</option>
+                        <option>Z - A</option>
+                      </select>
+                    </div>
+                    <div class="col-lg-8 col-md-6 col-sm-12 p-0">
+                      <input
+                        type="text"
+                        placeholder="Search..."
+                        class="form-control"
+                        id="search"
+                        name="search"
+                      />
+                    </div>
+                    <div class="col-lg-1 col-md-3 col-sm-12 p-0">
+                      <button type="submit" class="btn btn-base">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          class="feather feather-search"
+                        >
+                          <circle cx="11" cy="11" r="8"></circle>
+                          <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="d-flex justify-content-center mt-20 mb-50">
+      <div class="row">
+        <div
+          class="col-md-4 mt-3 mb-2"
+          v-for="product in products"
+          :key="product.id"
+        >
+          <div class="card">
+            <div class="card-body">
+              <div class="card-img-actions">
+                <img
+                  :src="product.image"
+                  class="card-img img-fluid"
+                  width="96px"
+                  height="350px"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div class="card-body bg-light text-center">
+              <div class="mb-2">
+                <h6 class="font-weight-semibold mb-2">
+                  {{ product.name }}
+                </h6>
+              </div>
+              <h3 class="mb-0 font-weight-semibold">
+                Price: ${{ product.price }}
+              </h3>
+              <button type="button" class="btn bg-cart" @click="count += 1">
+                Add to cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      brand: "AppleStore",
+      count: 0,
+      products: [
+        {
+          id: 1,
+          image: require("@/assets/iphone-x.jpg"),
+          name: "Iphone XSMax 64GB",
+          price: 500,
+        },
+        {
+          id: 2,
+          image: require("@/assets/iphone-11.jpg"),
+          name: "Iphone 11 Pro Max 64GB",
+          price: 1000,
+        },
+        {
+          id: 3,
+          image: require("@/assets/iphone-12.jpg"),
+          name: "Iphone 12 Pro Max 128GB",
+          price: 1500,
+        },
+        {
+          id: 4,
+          image: require("@/assets/iphone-x.jpg"),
+          name: "Iphone XSMax 256GB",
+          price: 700,
+        },
+        {
+          id: 5,
+          image: require("@/assets/iphone-11.jpg"),
+          name: "Iphone 11 Pro Max 256GB",
+          price: 1200,
+        },
+        {
+          id: 6,
+          image: require("@/assets/iphone-12.jpg"),
+          name: "Iphone 12 Pro Max 256GB",
+          price: 1900,
+        },
+      ],
+    };
+  },
+};
+</script>
+<style lang="css">
+body {
+  background: linear-gradient(to right, #c04848, #480048);
+  min-height: 100vh;
+}
+h1 {
+  color: #fff;
+}
+.bg-cart {
+  margin-top: 30px;
+  background-color: orange;
+  color: #fff;
+}
+
+.bg-cart:hover {
+  color: #fff;
+  opacity: .8;
+}
+
+.bg-buy {
+  background-color: green;
+  color: #fff;
+  padding-right: 29px;
+}
+
+.bg-buy:hover {
+  opacity: .8;
+  color: #fff;
+}
+.mt-50 {
+  margin-top: 100px;
+}
+</style>
---
+++ HOMEWORK/Day12VueJSTemplate/hello-world2/src/App.vue
@@ -0,0 +1,294 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="row">
+        <div class="col-md-8 cart">
+          <div class="title">
+            <div class="row">
+              <div class="col">
+                <h4>
+                  <b>{{ brand }}</b>
+                </h4>
+              </div>
+              <div class="col align-self-center text-right text-muted">
+                3 items
+              </div>
+            </div>
+          </div>
+          <div
+            class="row border-top border-bottom"
+            v-for="product in products"
+            :key="product.id"
+          >
+            <div class="row main align-items-center">
+              <div class="col-2">
+                <img class="img-fluid" :src="product.image" alt="" />
+              </div>
+              <div class="col">
+                <div class="row text-muted">Name:</div>
+                <div class="row">{{ product.name }}</div>
+              </div>
+              <div class="col">
+                <a href="#">-</a><a href="#">1</a><a href="#">+</a>
+              </div>
+              <div class="col">
+                ${{ product.price }}
+                <span class="close"><a href="">&#10005;</a></span>
+              </div>
+            </div>
+          </div>
+          <div class="back-to-shop">
+            <a href="#">&leftarrow;</a
+            ><span class="text-muted">Back to shop</span>
+          </div>
+        </div>
+        <div class="col-md-4 summary">
+          <div>
+            <h5><b>Summary</b></h5>
+          </div>
+          <hr />
+          <div class="row">
+            <div class="col" style="padding-left:0;">ITEMS 3</div>
+            <div class="col text-right">$2000</div>
+          </div>
+          <form>
+            <p>SHIPPING</p>
+            <select>
+              <option class="text-muted"
+                >Standard-Delivery- ${{ delivery }}</option
+              >
+            </select>
+            <p>GIVE CODE</p>
+            <input id="code" placeholder="Enter your code" />
+          </form>
+          <div
+            class="row"
+            style="border-top: 1px solid rgba(0,0,0,.1); padding: 2vh 0;"
+          >
+            <div class="col">TOTAL PRICE</div>
+            <div class="col text-right"> $2005 </div>
+          </div>
+          <button class="btn">CHECKOUT</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      brand: "AppleStore",
+      delivery: 5,
+      count: 0,
+      selected: 0,
+      products: [
+        {
+          id: 1,
+          image: require("@/assets/iphone-x.jpg"),
+          name: "Iphone XSMax 64GB",
+          price: 500,
+        },
+        {
+          id: 2,
+          image: require("@/assets/iphone-11.jpg"),
+          name: "Iphone 11 Pro Max 64GB",
+          price: 1000,
+        },
+        {
+          id: 3,
+          image: require("@/assets/iphone-12.jpg"),
+          name: "Iphone 12 Pro Max 128GB",
+          price: 1500,
+        },
+      ],
+    };
+  },
+};
+</script>
+<style lang="css">
+body {
+  background: linear-gradient(to right, #c04848, #480048);
+  min-height: 100vh;
+  vertical-align: middle;
+  display: flex;
+  font-family: sans-serif;
+  font-weight: bold;
+}
+
+.title {
+  margin-bottom: 5vh;
+}
+
+.card {
+  margin: auto;
+  margin-top: 10%;
+  max-width: 950px;
+  width: 90%;
+  box-shadow: 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  border-radius: 1rem;
+  border: transparent;
+}
+
+@media (max-width: 767px) {
+  .card {
+    margin: 3vh auto;
+  }
+}
+
+.cart {
+  background-color: #fff;
+  padding: 4vh 5vh;
+  border-bottom-left-radius: 1rem;
+  border-top-left-radius: 1rem;
+}
+
+@media (max-width: 767px) {
+  .cart {
+    padding: 4vh;
+    border-bottom-left-radius: unset;
+    border-top-right-radius: 1rem;
+  }
+}
+
+.summary {
+  background-color: #ddd;
+  border-top-right-radius: 1rem;
+  border-bottom-right-radius: 1rem;
+  padding: 4vh;
+  color: rgb(65, 65, 65);
+}
+
+@media (max-width: 767px) {
+  .summary {
+    border-top-right-radius: unset;
+    border-bottom-left-radius: 1rem;
+  }
+}
+
+.summary .col-2 {
+  padding: 0;
+}
+
+.summary .col-10 {
+  padding: 0;
+}
+
+.row {
+  margin: 0;
+}
+
+.title b {
+  font-size: 1.5rem;
+}
+
+.main {
+  margin: 0;
+  padding: 2vh 0;
+  width: 100%;
+}
+
+.col-2,
+.col {
+  padding: 0 1vh;
+}
+
+a {
+  padding: 0 1vh;
+}
+
+.close {
+  margin-left: auto;
+  font-size: 0.7rem;
+}
+
+img {
+  width: 5rem;
+}
+
+.back-to-shop {
+  margin-top: 4.5rem;
+}
+
+h5 {
+  margin-top: 4vh;
+}
+
+hr {
+  margin-top: 1.25rem;
+}
+
+form {
+  padding: 2vh 0;
+}
+
+select {
+  border: 1px solid rgba(0, 0, 0, 0.137);
+  padding: 1.5vh 1vh;
+  margin-bottom: 4vh;
+  outline: none;
+  width: 100%;
+  background-color: rgb(247, 247, 247);
+}
+
+input {
+  border: 1px solid rgba(0, 0, 0, 0.137);
+  padding: 1vh;
+  margin-bottom: 4vh;
+  outline: none;
+  width: 100%;
+  background-color: rgb(247, 247, 247);
+}
+
+input:focus::-webkit-input-placeholder {
+  color: transparent;
+}
+
+.btn {
+  background-color: #000;
+  border-color: #000;
+  color: white;
+  width: 100%;
+  font-size: 0.7rem;
+  margin-top: 4vh;
+  padding: 1vh;
+  border-radius: 0;
+}
+
+.btn:focus {
+  box-shadow: none;
+  outline: none;
+  box-shadow: none;
+  color: white;
+  -webkit-box-shadow: none;
+  -webkit-user-select: none;
+  transition: none;
+}
+
+.btn:hover {
+  color: white;
+}
+
+a {
+  color: black;
+}
+
+a:hover {
+  color: black;
+  text-decoration: none;
+}
+
+#code {
+  background-image: linear-gradient(
+      to left,
+      rgba(255, 255, 255, 0.253),
+      rgba(255, 255, 255, 0.185)
+    ),
+    url("https://img.icons8.com/small/16/000000/long-arrow-right.png");
+  background-repeat: no-repeat;
+  background-position-x: 95%;
+  background-position-y: center;
+}
+</style>
